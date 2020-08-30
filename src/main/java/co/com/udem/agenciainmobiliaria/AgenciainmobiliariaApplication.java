ad31c8df957f8d0d0b4543576fc67603fa4697c6
Patricia Carmona <patriciacr0107@gmail.com>
2020-08-30 16:34:24 -0500

    Se adiciona balanceo de cargas

--- src/main/java/co/com/udem/agenciainmobiliaria/AgenciainmobiliariaApplication.java
+++ src/main/java/co/com/udem/agenciainmobiliaria/AgenciainmobiliariaApplication.java
@@ -3,6 +3,7 @@ package co.com.udem.agenciainmobiliaria;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +14,7 @@ import co.com.udem.agenciainmobiliaria.util.ConvertUsuario;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AgenciainmobiliariaApplication {
 
 	public static void main(String[] args) {
